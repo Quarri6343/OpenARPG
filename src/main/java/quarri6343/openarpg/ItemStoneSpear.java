840290e1fa29ef37a0d29b91d03fbff177591497
くぁーりぃ <kaito6343@gmail.com>
2024-02-13 06:18:07 +0900

    init commit 現在実装済みの仕様 ・カスタム攻撃力 ・右クリックすると攻撃力が上昇するテストアイテム ・トレハンの醍醐味のカスタムルートテーブル(仮) ・modernuiとの基本的な互換性

---
+++ src/main/java/quarri6343/openarpg/ItemStoneSpear.java
@@ -0,0 +1,78 @@
+package quarri6343.openarpg;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.antlr.v4.runtime.misc.MultiMap;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ItemStoneSpear extends Item {
+
+    private static final int DAMAGE = 5;
+
+    public ItemStoneSpear(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.openarpg.stone_spear"));
+        
+        if(pStack.getTag() == null){
+            pStack.getOrCreateTag().putInt("Damage", DAMAGE);
+            pStack.getTag().putInt("HideFlags", 2);
+        }
+        int currentDamage = pStack.getTag().getInt("Damage");
+        pTooltipComponents.add(Component.translatable("攻撃力: " + currentDamage));
+        
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack pStack = pPlayer.getItemInHand(pUsedHand);
+
+        if(pStack.getTag() == null){
+            pStack.getOrCreateTag().putInt("Damage", DAMAGE);
+            pStack.getTag().putInt("HideFlags", 2);
+        }
+        int currentDamage = pStack.getTag().getInt("Damage");
+        CompoundTag tag = pStack.getOrCreateTag();
+        tag.putInt("Damage", currentDamage + 1);
+        pStack.setTag(tag);
+        
+        return InteractionResultHolder.success(pPlayer.getItemInHand(pUsedHand));
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+        final Multimap<Attribute, AttributeModifier> modifiers = HashMultimap.create();
+        if(slot != EquipmentSlot.MAINHAND)
+            return modifiers;
+        
+        if(stack.getTag() == null){
+            stack.getOrCreateTag().putInt("Damage", DAMAGE);
+        }
+        int currentDamage = stack.getTag().getInt("Damage");
+        modifiers.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", currentDamage - 1, AttributeModifier.Operation.ADDITION));
+        return modifiers;
+    }
+}
---
+++ src/main/java/quarri6343/openarpg/OpenARPG.java
@@ -0,0 +1,85 @@
+package quarri6343.openarpg;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+import static quarri6343.openarpg.CreativeTabInit.addToTab;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(OpenARPG.MODID)
+public class OpenARPG {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "openarpg";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "OpenARPG" namespace
+
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static final RegistryObject<Item> STONE_SPEAR = addToTab(ITEMS.register("stone_spear", () -> new ItemStoneSpear(new Item.Properties().stacksTo(1))));
+    
+    public OpenARPG() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ITEMS.register(modEventBus);
+        CreativeTabInit.TABS.register(modEventBus);
+    }
+
+    private void registerProviders(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+        PackOutput packOutput = gen.getPackOutput();
+        ExistingFileHelper fileHelper = event.getExistingFileHelper();
+    }
+    
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
