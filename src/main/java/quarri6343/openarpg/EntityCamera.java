65478373d032cb857e4b6e21c3f2d7b29b798d1b
くぁーりぃ <kaito6343@gmail.com>
2024-02-13 14:03:59 +0900

    3人称でカメラが俯瞰視点になるように変更

---
+++ src/main/java/quarri6343/openarpg/EntityCamera.java
@@ -0,0 +1,30 @@
+package quarri6343.openarpg;
+
+import net.minecraft.client.Camera;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class EntityCamera extends Entity {
+
+    public EntityCamera(EntityType<EntityCamera> p_19870_, Level p_19871_) {
+        super(p_19870_, p_19871_);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+
+    }
+
+    @Override
+    protected void readAdditionalSaveData(CompoundTag pCompound) {
+        
+    }
+
+    @Override
+    protected void addAdditionalSaveData(CompoundTag p_20139_) {
+
+    }
+
+}
--- src/main/java/quarri6343/openarpg/OpenARPG.java
+++ src/main/java/quarri6343/openarpg/OpenARPG.java
@@ -4,13 +4,24 @@ import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.client.event.ViewportEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -36,8 +47,13 @@ public class OpenARPG {
     // Create a Deferred Register to hold Blocks which will all be registered under the "OpenARPG" namespace
 
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
     public static final RegistryObject<Item> STONE_SPEAR = addToTab(ITEMS.register("stone_spear", () -> new ItemStoneSpear(new Item.Properties().stacksTo(1))));
+
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MODID);
+
+    public static final RegistryObject<EntityType<EntityCamera>> CAMERA = ENTITY_TYPES.register("camera", () -> EntityType.Builder.of(EntityCamera::new, MobCategory.MISC).build(MODID + ":camera"));
+    
+    private static EntityCamera cameraInstance;
     
     public OpenARPG() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -49,13 +65,8 @@ public class OpenARPG {
         MinecraftForge.EVENT_BUS.register(this);
 
         ITEMS.register(modEventBus);
+        ENTITY_TYPES.register(modEventBus);
         CreativeTabInit.TABS.register(modEventBus);
-    }
-
-    private void registerProviders(GatherDataEvent event) {
-        DataGenerator gen = event.getGenerator();
-        PackOutput packOutput = gen.getPackOutput();
-        ExistingFileHelper fileHelper = event.getExistingFileHelper();
     }
     
     private void commonSetup(final FMLCommonSetupEvent event) {
@@ -70,6 +81,40 @@ public class OpenARPG {
         LOGGER.info("HELLO from server starting");
     }
 
+    @Mod.EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
+    public static class ClientEventHandler {
+        @SubscribeEvent
+        public static void onCameraRotation(ViewportEvent.ComputeCameraAngles event) {
+            if(Minecraft.getInstance().options.getCameraType().isFirstPerson()){
+                Minecraft.getInstance().setCameraEntity(Minecraft.getInstance().player);
+            }
+            else{
+                if(Minecraft.getInstance().getCameraEntity() instanceof Player){
+                    cameraInstance.setXRot(45);
+                    cameraInstance.setYRot(45);
+                    Vec3 eyePos = Minecraft.getInstance().getCameraEntity().getEyePosition();
+                    event.getCamera().tick();
+                    cameraInstance.setPosRaw(eyePos.x, eyePos.y, eyePos.z);
+                    cameraInstance.setOldPosAndRot();
+                    Minecraft.getInstance().setCameraEntity(cameraInstance);
+                }
+            }
+        }
+        
+        @SubscribeEvent
+        public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event){
+            cameraInstance = CAMERA.get().create(event.getEntity().level());
+        }
+
+        @SubscribeEvent
+        public static void onPlayerLogout(PlayerEvent.PlayerLoggedOutEvent event){
+            if(cameraInstance != null){
+                cameraInstance.remove(Entity.RemovalReason.DISCARDED);
+                cameraInstance = null;
+            }
+        }
+    }
+    
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
@@ -77,9 +122,7 @@ public class OpenARPG {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event)
         {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+            
         }
     }
 }
