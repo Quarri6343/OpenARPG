517ca534fab42de54bda7114058e05e3d15bce90
くぁーりぃ <kaito6343@gmail.com>
2024-02-17 20:24:19 +0900

    ドロップアイテムに被さってアイテム名が表示されるように

--- src/main/java/quarri6343/openarpg/DroppedItemEventHandler.java
+++ src/main/java/quarri6343/openarpg/DroppedItemEventHandler.java
@@ -1,6 +1,12 @@
 package quarri6343.openarpg;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.vertex.PoseStack;
+import icyllis.modernui.mc.forge.MuiForgeApi;
+import icyllis.modernui.mc.text.TextLayoutEngine;
+import icyllis.modernui.mc.text.mixin.MixinFontRenderer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
@@ -10,6 +16,7 @@ import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.opengl.GL11;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,18 +39,38 @@ public class DroppedItemEventHandler {
 
     @SubscribeEvent
     public static void onRenderOverlay(RenderGuiOverlayEvent event) {
-        //debug
         if (Minecraft.getInstance().options.getCameraType().isFirstPerson() || Minecraft.getInstance().screen != null) {
             return;
         }
         
-        
+        float scale = 0.5f;
         itemEntityList.forEach(entity -> {
             Vec3 screenPos = ProjectionUtil.worldToScreen(entity.getPosition(event.getPartialTick()));
+            String itemName = entity.getItem().getDisplayName().getString();
+            int textWidth = Minecraft.getInstance().font.width(itemName);
+            int textHeight = Minecraft.getInstance().font.lineHeight;
+            PoseStack pose = event.getGuiGraphics().pose();
             
-            event.getGuiGraphics().fill((int) screenPos.x - 5, (int) screenPos.y - 5, (int) screenPos.x + 5, (int) screenPos.y + 5, 0xFF000000);
+            {
+                pose.pushPose();
+                pose.scale(scale, scale, 1);
+                int x = (int) (((int)screenPos.x - textWidth * scale / 2 ) / scale);
+                int y = (int) (((int)screenPos.y - textHeight * scale / 2 ) / scale);
+                event.getGuiGraphics().drawString(Minecraft.getInstance().font, itemName, x, y, 0xFF000000);
+                pose.popPose();
+            }
+            {
+                pose.pushPose();
+                pose.scale(scale, scale, 1);
+                int minX = (int) (((int)screenPos.x - textWidth * scale / 2 ) / scale);
+                int minY = (int) (((int)screenPos.y - textHeight * scale / 2 ) / scale);
+                int maxX = (int) (((int)screenPos.x + textWidth * scale / 2 ) / scale);
+                int maxY = (int) (((int)screenPos.y + textHeight * scale / 2 ) / scale);
+                event.getGuiGraphics().fill(minX - 3, minY - 3,
+                        maxX + 3, maxY + 3, 0xFFFFFFFF);
+                pose.popPose();
+            }
         });
-        
         itemEntityList.clear();
     }
 }
