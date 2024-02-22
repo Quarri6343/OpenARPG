618d660e820adb66698d4eeff24a68276222078c
くぁーりぃ <kaito6343@gmail.com>
2024-02-22 12:20:52 +0900

    空のコンテナを登録だけする

---
+++ src/main/java/quarri6343/openarpg/ui/ExampleSidedInventoryScreen.java
@@ -0,0 +1,33 @@
+package quarri6343.openarpg.ui;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import org.jetbrains.annotations.NotNull;
+import quarri6343.openarpg.OpenARPG;
+
+public class ExampleSidedInventoryScreen extends AbstractContainerScreen<ExampleSidedInventoryMenu> {
+    private static final ResourceLocation TEXTURE =
+            new ResourceLocation(OpenARPG.MODID, "textures/gui/example_sided_inventory.png");
+
+    public ExampleSidedInventoryScreen(ExampleSidedInventoryMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
+        super(pMenu, pPlayerInventory, pTitle);
+
+        this.imageWidth = 176;
+        this.imageHeight = 166;
+    }
+
+    @Override
+    protected void renderBg(@NotNull GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
+//        renderTransparentBackground(pGuiGraphics);
+        pGuiGraphics.blit(TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
+    }
+
+    @Override
+    public void render(@NotNull GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+        renderTooltip(pGuiGraphics, pMouseX, pMouseY);
+    }
+}
